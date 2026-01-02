5391ccd4fd327d1c141b299c3d61dbb30a9fb0be
wc-su <theblissout@gmail.com>
2026-01-03 00:59:49 +0800

    feat: 更新選單畫面

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import { Link } from "react-router"
+import { Link } from "react-router";
 
 function App() {
-
   return (
     <div className="container py-5">
       <h1 className="text-center mb-5">2025 六角學院 React 前端框架學程</h1>
-      <div className="row">
-        <div className="col text-center">
-          <Link to="/task2" className="btn btn-primary">Go to Task 2</Link>
-        </div>
-        {/* <div className="col">
-          <Link to="/task3">Go to Task 3</Link>
-        </div> */}
-      </div>
+      <ul className="list-unstyled">
+        <li className="text-center mb-3">
+          <Link to="/task2" className="btn btn-primary mx-auto">
+            第二週 - RESTful API 串接
+          </Link>
+        </li>
+        <li className="text-center mb-3">
+          <Link
+            to="/task3"
+            className="btn btn-outline-primary mx-auto disabled"
+          >
+            第三週 - 熟練 React.js
+          </Link>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
